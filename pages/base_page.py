e1f8710beba36b18b65315443570f9953e5298a5
Abdulmalik Yermekbay <malikdemaximus@users.noreply.github.com>
2025-05-03 12:02:29 +0500

    Create base_page.py

---
+++ pages/base_page.py
@@ -0,0 +1,19 @@
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self, url):
+        self.driver.get(url)
+
+    def find(self, locator):
+        return self.driver.find_element(*locator)
+
+    def click(self, locator):
+        self.find(locator).click()
+
+    def type(self, locator, text):
+        self.find(locator).clear()
+        self.find(locator).send_keys(text)
+
+    def get_text(self, locator):
+        return self.find(locator).text
